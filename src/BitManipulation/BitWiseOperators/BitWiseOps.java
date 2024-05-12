c5f6d5faa039a4e229bcbab782d3004a7ce900a7
bgn2k <bgn.cbe@gmail.com>
2024-05-12 21:03:43 +0530

    Added Bitwise Operators

---
+++ src/BitManipulation/BitWiseOperators/BitWiseOps.java
@@ -0,0 +1,9 @@
+package BitManipulation.BitWiseOperators;
+
+public class BitWiseOps {
+    public static void main(String[] args) {
+        System.out.println(5 | 9);
+        System.out.println(5 & 9);
+        System.out.println(5 ^ 9);
+    }
+}
