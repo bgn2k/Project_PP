49628adc7bcece8a1782326379cbf32f8eabaa89
bgn2k <bgn.cbe@gmail.com>
2024-08-07 06:33:00 +0530

    Added efficient approach for leaders in an array pgm

--- src/Arrays/ArrayOperations/LeadersInAnArray/LeadersInAnArray.java
+++ src/Arrays/ArrayOperations/LeadersInAnArray/LeadersInAnArray.java
@@ -6,6 +6,20 @@ public class LeadersInAnArray {
     public static void main(String[] args) {
         int arr[] = getInput.getArrayInput();
         executeNaiveMethod(arr);
+        executeEfficientMethod(arr);
+    }
+
+    private static void executeEfficientMethod(int[] arr) {
+        int n = arr.length;
+        System.out.println("\nEfficient Method Answer: ");
+        System.out.print(arr[n - 1]+" ");
+        int leader = arr[n - 1];
+        for (int i = n-2; i >= 0; i--) {
+            if(arr[i] > leader){
+                System.out.print(arr[i]+" ");
+                leader = arr[i];
+            }
+        }
     }
 
     private static void executeNaiveMethod(int[] arr) {
@@ -13,16 +27,17 @@ public class LeadersInAnArray {
         int n = arr.length;
 //        int result[] = new int[n];
         for(int i = 0; i< n; i++){
-            boolean flag = false;
+            boolean flag = true;
             for (int j = i+1; j < n; j++) {
-                if(arr[i] <= arr[j]){
-                    flag = true;
+                if(arr[j] >= arr[i]){
+                    flag = false;
                     break;
                 }
             }
-            if(!flag){
+            if(flag){
                 System.out.print(arr[i]+" ");
             }
         }
     }
+
 }
